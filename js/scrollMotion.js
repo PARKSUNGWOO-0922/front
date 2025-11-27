e73a33afdea5deaeb7508e96e4c37fd58ddba153
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2025-11-27 21:06:17 +0900

    1127

--- js/scrollMotion.js
+++ js/scrollMotion.js
@@ -1,68 +1,68 @@
- 
-   
-        $(window).scroll(function () {
-            //변수 선언
-            const windowHeight = $(window).height();
-            let windowScrollTop = $(window).scrollTop();
-            const speed = 1500, 
-                  easing = "easeOutExpo", 
-                  pos = 0; 
+/*  기존 scrollMotion2 수정하여 about 의 문단및 사진 스크롤 추가작업 */
 
-            $(".motion").each(function(i,el){
-                //position():포지션이 지정된 가장 가까운 조상 요소를 기준으로 지정한다.
-                const elementTop = $(el).position().top;
-                //offset1: .motion의 움직이기 시작하는 시점:숫자가클수록 더 많이 스크롤해야 동작한다.
-                //offset2: .motion의 시작 위치
-                let offset1 ,offset2 ,speed;
+$(window).scroll(function () {
+    //변수 선언
+    const windowHeight = $(window).height();
+    let windowScrollTop = $(window).scrollTop();
+    const easing = "easeOutExpo", pos = 0;
 
-                switch (i) {
-                    case 0 : 
-                        offset1 = 800;
-                        offset2 = 300;  
-                        speed = 2000;   
-                        break;
-                    case 1:
-                    case 2:
-                        offset1 = 800;
-                        offset2 = 600;  
-                        speed = 1000;  
-                    break;
-                    default:
-                        offset1 = 1800;
-                        offset2 = 600;  
-                        speed = 400;   
-                }
+    $(".motion").each(function (i, el) {
+        //position():포지션이 지정된 가장 가까운 조상 요소를 기준으로 지정한다.
+        const elementTop = $(el).position().top;
+        //offset1: .motion의 움직이기 시작하는 시점:숫자가클수록 더 많이 스크롤해야 동작한다.
+        //offset2: .motion의 시작 위치
+        let offset1, offset2, speed;
 
-
-                if ( i < 1){
-                    offset1 = 800;
-                    offset2 = 300;  
-                    speed = 2000;   
-
-                } else if ( i < 3) {
-                    offset1 = 800;
-                    offset2 = 600;  
-                    speed = 1000;   
-                } else {
-                    offset1 = 1800;
-                    offset2 = 600;  
-                    speed = 400;   
-
-                }
-
-                if (windowScrollTop > elementTop - windowHeight  +offset1) {
-                $(el).stop().animate({ top: pos, opacity: 1 }, (speed * (i + 1)) / 2, easing);
-                }
-                else {
-                    $(el).stop().animate({ top: pos + offset2, opacity: 0 }, speed, easing);
-                }
-            });
-
-            if ( windowScrollTop > 0) {
-                $("#header").addClass("active");
-            } else {
-          $("#header").removeClass("active");
+        switch (i) {
+            case 0:
+                offset1 = 800;
+                offset2 = 300;
+                speed = 2000;
+                break;
+            case 1:
+            case 2:
+                offset1 = 800;
+                offset2 = 600;
+                speed = 1000;
+                break;
+            default:
+                offset1 = 1800;
+                offset2 = 600;
+                speed = 400;
         }
-   });//$(window).scroll
 
-   
+        // if ( i < 1){
+        //     offset1 = 800;
+        //     offset2 = 300;  
+        //     speed = 2000;   
+
+        // } else if ( i < 3) {
+        //     offset1 = 800;
+        //     offset2 = 600;  
+        //     speed = 1000;   
+        // } else {
+        //     offset1 = 1800;
+        //     offset2 = 600;  
+        //     speed = 400;   
+
+        // }
+
+        // console.log(elementTop);   
+        if (windowScrollTop > elementTop - windowHeight + offset1) {
+            $(el).stop().animate({ top: pos, opacity: 1 }, (speed * (i + 1)) / 2, easing);
+        }
+        else {
+            $(el).stop().animate({ top: pos + offset2, opacity: 0 }, speed, easing);
+        }
+    });
+
+    // 창의 스크롤 양이 0보다 크면
+    if (windowScrollTop > 0) {
+        // 헤더 요소에 active 클래스 추가
+        $("#header").addClass("active");
+    } else {
+        // 헤더 요소에 active 클래스 제거
+        $("#header").removeClass("active");
+    }
+});//$(window).scroll
+
