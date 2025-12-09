d0fa1440432dc566edef38a84254a5c8ec038fcf
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2025-12-09 15:55:59 +0900

    3차수정_아바타

---
+++ mysite_bs4/js/data.js
@@ -0,0 +1,40 @@
+// 카드 데이터 배열
+// https://www.dummyimage.com/ 더미이미지(profile..)
+const datas = [
+    {
+        rank: 1,
+        poster: "assets/img/p1.jpg",
+        title: '???', 
+        task: 'TE',
+        description: [],
+        worktime: "240H",
+        siteurl: "#"
+    },
+    {
+        rank: 2,
+        poster: "assets/img/project2_thumb.png",
+        title: "",
+        task: 'PE',
+        description: ["풀 페이지 방식", "적응형 웹 사이트", "페이지수: 5장", "기술 스택: HTML, CSS, JS(jQuery) 등"],
+        worktime: "40H",
+        siteurl: "https://PARKSUNGWOO-0922.github.io/jfs/project"
+    },
+    {
+        rank: 3,
+        poster: "assets/img/project3_thumb.png",
+        title: "메가박스",
+        task: "PE",
+        description: ["준비중..."],
+        worktime: "16H",
+        siteurl: "#"
+    },
+    {
+        rank: 4,
+        poster: "https://www.dummyimage.com/400x600/000/fff&text=+preparing...",
+        title: "해태제과",
+        task: "PE",
+        description: ["준비중..."],
+        worktime: "240H",
+        siteurl: "#"
+    },
+];
---
+++ mysite_bs4/js/scrollMotion2.js
@@ -0,0 +1,42 @@
+ 
+   
+        /* about 문단및사진 스크롤 추가하기전 코딩자료 
+            work 의 이미지 4개 차등 스크롤 과 헤더 스크롤시 배경변경
+        */
+        
+        $(window).scroll(function () {
+            //변수 선언
+            const windowHeight = $(window).height();
+            let windowScrollTop = $(window).scrollTop();
+            const speed = 1000, 
+                  easing = "easeOutExpo", 
+                  pos = 0, 
+                  offset = 2.5;
+
+            // 오른쪽 상단에 스크롤 양과 창 높이 값 출력
+           // console.log(windowScrollTop + " , " + windowHeight);
+
+            if (windowScrollTop > windowHeight * offset) {
+                //참일 경우
+                // i는 0, 1, 2, 3
+                for (let i = 0; i < 4; i++) {
+                    $(".motion").eq(i).stop().animate({ top: pos, opacity: 1 }, (speed * (i + 1)) / 2, easing);
+                }
+            } 
+            else {
+                //거짓일 경우
+                $(".motion").stop().animate({ top: pos + 600, opacity: 0 }, speed, easing);
+            }
+            //------------------------header 배경색--------------------------------------------------------------
+            //윈도우의 스크롤 이벤트가 발생하면
+            //창의 스크롤 양이 0보다 크면
+            if ( windowScrollTop > 0) {
+                //헤더 요소에 active클래스 추가
+                $("#header").addClass("active");//개발자 모드에서 확인가능
+            } else {
+                //헤더 요소에 active클래스 제거
+          $("#header").removeClass("active");
+        }
+   });//$(window).scroll
+
+   
